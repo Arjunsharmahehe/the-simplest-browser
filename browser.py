c0964ee051b8888a2e6440c9fc1001eac38406a3
arjunsharma <whocaresaboutarjun@gmail.com>
2025-02-01 17:02:59 +0530

    add comments to the simplest browser

--- browser.py
+++ browser.py
@@ -1,15 +1,24 @@
 from socket import *
 
+# inits a socket
 mysocket = socket(AF_INET, SOCK_STREAM)
+
+# forms the connection with the server (domain name, port )
 mysocket.connect(('data.pr4e.org', 80))
+
+# the telnet command that we send
 command = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+
+# sending that command to the server
 mysocket.send(command)
 
 while True:
+    # reading the data in small packages (the server might be slow)
     data = mysocket.recv(512)
     if len(data) < 1:
         break
     else:
         print(data.decode(), end='')
 
+# closing the socket from our side
 mysocket.close()
